52d2230bc1df078e65f8825f95ba0db4deed5e20
Willy Junior <willyjunni@gmail.com>
2024-11-21 15:12:46 -0400

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,10 @@
+nombre  = input("ingrse su nombre  ")
+edad = input("ingrece su edad  ")
+cedula = input("ingrese su cedula diog si tiene jaajaja"  )
+print("hola,",nombre, "y tienes," ,edad,"su cedula es",cedula)
+
+
+
+import datetime
+fecha = datetime.datetime.now()
+print("fecha y hora actual", fecha)
